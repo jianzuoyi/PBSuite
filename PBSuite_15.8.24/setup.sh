50a5509f2ed2b5e94058285ab3a57bfc742021f4
Zuoyi Jian <jianzuoyi@qq.com>
2017-11-06 10:33:53 +0800

    change dir

--- PBSuite_15.8.24/setup.sh
+++ PBSuite_15.8.24/setup.sh
@@ -3,11 +3,11 @@
 #If you use a virtual env - source it here
 #source /hgsc_software/PBSuite/pbsuiteVirtualEnv/bin/activate
 #source /its1/GB_BT2/jianzuoyi/biosoft/anaconda/envs/pbjelly/bin/activate
-source activate pbjelly
+#source activate pbjelly
 
 #This is the path where you've install the suite.
 #export SWEETPATH=/stornext/snfs5/next-gen/scratch/english/Jelly/DevJelly/trunk
-export SWEETPATH=/its1/GB_BT2/jianzuoyi/pipeline/biosoft/01.Assembly/PBSuite/PBSuite_15.8.24
+export SWEETPATH=/its1/GB_BT2/jianzuoyi/biosoft/PBSuite/PBSuite_15.8.24
 
 #for python modules 
 export PYTHONPATH=$PYTHONPATH:$SWEETPATH
@@ -16,4 +16,5 @@ export PYTHONPATH=$PYTHONPATH:$SWEETPATH
 PATH=$PATH:$SWEETPATH/bin/
 
 #for blasr 1.3.1
-export PATH=/its1/GB_BT2/jianzuoyi/biosoft/smrtanalysis_2.3.0.140936_bin:$PATH
+#export PATH=/its1/GB_BT2/jianzuoyi/biosoft/smrtanalysis_2.3.0.140936_bin:$PATH
+#export PATH=/its1/GB_BT2/jianzuoyi/biosoft/smrtanalysis_2.3.0.140936_bin:$PATH
